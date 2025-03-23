312e6ff888cb486d55b95cfe1ed62c2629b9958f
d-sys.wiki <sjmule@proton.me>
2025-03-22 23:14:08 -0700

    chore: pass dummy value for postId

--- dist/index.js
+++ dist/index.js
@@ -31834,7 +31834,7 @@ try {
   const message = core.getInput('message');
   const communityId = core.getInput('community-id');
   console.log(message, communityId);
-  const postId = new uuidv4();
+  const postId = 666;
   core.setOutput("post-id", postId);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
